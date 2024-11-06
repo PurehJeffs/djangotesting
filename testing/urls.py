a4357f426c89e952b2e9cea1f20deb3e2db4dc24
Jeffs <ubernovaj64@gmail.com>
2024-11-06 15:06:18 +0800

    added ahhh

--- testing/urls.py
+++ testing/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 #hahaha
-# weh
+# ahhh
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('base.urls')),
